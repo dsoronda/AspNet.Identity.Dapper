8b11af09f30ae8c49afa233a1db5382e7826bcd1
dsoronda <dsoronda@gmail.com>
2014-05-29 14:21:52 +0200

    Init ... Add missing files

---
+++ src/AspNet.Identity.Dapper/AspNet.Identity.Dapper/DapperUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+
+namespace AspNet.Identity.Dapper
+{
+	/// <summary>
+	/// Dapper user.
+	/// </summary>
+	public class DapperUser<TKey> : IUser
+	{
+		public TKey UserId { get; set; }
+		public string UserName { get; set; }
+		public string PasswordHash { get; set; }
+
+		public string Id {
+			get {
+				return UserId.ToString ();
+			}
+		}
+	}
+}
+
